4d33527fefda83291c28ed290bd4ed105554e271
BlackCoder56 <blackcoda56@gmail.com>
2025-07-26 17:42:14 +0300

    Tailwindcss Layout

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,29 @@
 </script>
 
 <template>
-    <h1 class="text-8xl text-blue-700">MY TITLE</h1>
+    <h1 class="text-center text-lg text-green-400">Hello World</h1>
+
+    <div class="w-full h-10 bg-violet-200 border-violet-600 border-2 rounded-md my-4 p-2">
+      <p class="text-center font-mono font-extrabold">A div</p>
+    </div>
+
+    <!-- Layout -->
+     <div class="fixed w-10 h-10 bg-red-500 top-0"></div>
+
+     <div class="flex justify-between">
+      <div class="h-16 w-16 rounded-full bg-blue-500"></div>
+      <div class="h-16 w-16 rounded-full bg-blue-500"></div>
+      <div class="h-16 w-16 rounded-full bg-blue-500"></div>
+     </div>
+
+     <div class="grid grid-cols-3 gap-2">
+      <div class="bg-violet-500 h-12"></div>
+      <div class="bg-violet-500 h-12"></div>
+      <div class="bg-violet-500 h-12"></div>
+      <div class="bg-violet-500 h-12"></div>
+      <div class="bg-violet-500 h-12"></div>
+      <div class="bg-violet-500 h-12"></div>
+     </div>
 </template>
 
 <style scoped>
